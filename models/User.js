57875ba4e4a811a8381a4d4446142ac298f631d8
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-02-06 14:07:57 -0500

    set up blog User model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+// variables/installs set for user model 
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {}
+// add function to do password check
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+})
+
+module.exports = User; 
